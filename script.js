46fdbe286d041c50d54958181afba2dc285c5410
Илья Куракин <149002829+IV-Kurakin@users.noreply.github.com>
2023-11-25 13:39:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(document).mousemove(function(event){
+       $("#light").css({"top": event.pageY - 250, "left": event.pageX - 300}); 
+    }); 
+    
+    
+ });
+ 
